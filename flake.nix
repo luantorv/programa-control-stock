7c7653c2c298d06a1b95c3c0d7cba48462312d1e
Luis Reis Viera <luantorv@gmail.com>
2026-07-14 19:45:33 -0300

    Añadir flake para declarar el entorno de desarrollo

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Programa de Control de Stock para un Supermercado";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python312.withPackages (ps: with ps; [
+          # Aquí irían las librerías, en este caso no aplica
+        ]);
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            python
+          ];
+
+          #shellHook = ''
+          #  '';
+        };
+      }
+    );
+}
